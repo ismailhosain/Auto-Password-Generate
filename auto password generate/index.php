cb79ecea7cfe5fe712ee7234a922df49b9770cb7
ismailhosain <63832968+ismailhosain@users.noreply.github.com>
2021-07-03 22:34:29 +0600

    Add files via upload

---
+++ auto password generate/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Auto password generate</title>
+</head>
+<body>
+
+
+<?php 
+
+
+$str="ajisjd3345455@#$%0254";
+
+$strs=str_shuffle($str);
+
+$strs=substr($strs,0,8);
+
+
+
+
+
+?>
+
+<input type="text" value="<?php echo $strs; ?>">
+<input id="btntest" type="button" value="generate Your password" 
+       onclick="window.location.href = 'index.php'" />
+
+
+
+</body>
+</html>
